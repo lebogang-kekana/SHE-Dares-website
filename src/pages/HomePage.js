e60152d338d463b4613f336a8340083df66a8baa
lebogangK-girlcode <109577006+lebogangK-girlcode@users.noreply.github.com>
2022-09-12 15:49:51 +0200

    added home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function HomePage() {
+
+return (
+
+<div className="container">
+
+<h1>Hey from HomePage</h1>
+
+<p>This is your awesome HomePage subtitle</p>
+
+</div>
+
+);
+
+}
---
+++ src/pages/awsPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function awsPage() 
+{
+
+  return 
+   (
+
+    <>
+
+    <h1>Hello there user </h1>
+
+    <p>This is your awesome User Profile page</p>
+
+    </>
+   );
+
+}
